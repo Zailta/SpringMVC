945880946dc4d1db6ec30e36f487bd0cd9534236
Mohammad Manaan Bhat <mannanbhat60@gmail.com>
2023-08-19 14:51:36 +0530

    Thymeleaf

--- src/main/java/com/java/mvc/operations/SpringMVC/controller/ThymeleafController.java
+++ src/main/java/com/java/mvc/operations/SpringMVC/controller/ThymeleafController.java
@@ -1,13 +1,22 @@
 package com.java.mvc.operations.SpringMVC.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.java.mvc.operations.SpringMVC.Entity.UserEntity;
+import com.java.mvc.operations.SpringMVC.service.CRUDService;
+
 @RestController
 public class ThymeleafController {
+	@Autowired
+	CRUDService crudService;
 	private static ModelAndView modelAndView = new ModelAndView();
 
 	@GetMapping(value = "/loop")
@@ -37,6 +46,33 @@ public class ThymeleafController {
 		modelAndView.addObject("serviceInvoker", "Handler Class");
 		modelAndView.setViewName("thymleaf-form1");
 		return modelAndView;
+	}
+	
+	@GetMapping(value = "login")
+	public ModelAndView loginPage() {
+		modelAndView.setViewName("Thymeleaf-Login");
+		return modelAndView;
+	}
+	@PostMapping(value = "/loginUser")
+	public ModelAndView loginUser(@RequestParam String UserName, @RequestParam String password) {
+		System.out.println("---------------------------------------------------------");
+		ModelAndView modelAndView = new ModelAndView();
+		Optional<UserEntity> findUser = crudService.findUser(UserName);
+		findUser.ifPresent(userEntity -> {
+			System.out.println("---------------------------------------------------------");
+			if(userEntity.getUserName().equalsIgnoreCase(UserName) && userEntity.getPassword().equals(password)) {
+				modelAndView.addObject("UserName",userEntity.getUserName());
+				modelAndView.setViewName("Thymeleaf-Success");
+				
+				
+			} 
+			else {
+				modelAndView.setViewName("errorHandlerPage");
+			}
+		});
+		return modelAndView;
+		
+
 	}
 
 }
